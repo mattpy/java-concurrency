f65dea9cd776f9fb737d69119ab7af05ba8e526d
Matt <casper.angelfire@gmail.com>
2025-02-03 01:31:24 -0600

    Anno stuff

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,38 @@
+import java.lang.annotation.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Exchange e = new Exchange();
-        Producer p = new Producer(e);
-        Consumer c = new Consumer(e);
+    public static void main(String[] args) throws Exception {
+        Annotation[] annos = Main.class.getDeclaredMethod("foo").getDeclaredAnnotations();
+        for (Annotation a : annos) {
+            System.out.println(a);
+        }
 
-        new Thread(p).start();
-        new Thread(c).start();
+        MyAnnoContainer con = Main.class.getDeclaredMethod("foo").getDeclaredAnnotation(MyAnnoContainer.class);
+        System.out.println(con);
+    }
+
+    @MyAnnoContainer(
+            value = {
+                    @MyAnno(name = "Anno 1"),
+                    @MyAnno(name = "Anno 2")
+            },
+            description = "The coolest"
+    )
+    @MyAnno(name = "Anno 3")
+    static void foo() {
+
     }
 }
 
-class Exchange {
-    private String message = "";
-    private boolean hasMessage = false;
-
-    public synchronized void putMessage(String message) {
-        while (hasMessage) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-        this.message = message;
-        hasMessage = true;
-        notifyAll();
-    }
-
-    public synchronized String getMessage() {
-        while (!hasMessage) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-        hasMessage = false;
-        notifyAll();
-        return message;
-    }
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(MyAnnoContainer.class)
+@interface MyAnno {
+    String name();
 }
 
-class Producer implements Runnable {
-    private final Exchange e;
+@Retention(RetentionPolicy.RUNTIME)
+@interface MyAnnoContainer {
+    MyAnno[] value();
 
-    Producer(Exchange ex) {
-        e = ex;
-    }
-
-    @Override
-    public void run() {
-        String[] messages = {"Hello", "world", "goodnight", "moon"};
-        try {
-            for (String message : messages) {
-                e.putMessage(message);
-                Thread.sleep(500);
-            }
-            e.putMessage("STOP");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt();
-        }
-    }
-}
-
-class Consumer implements Runnable {
-    private final Exchange e;
-
-    Consumer(Exchange ex) {
-        e = ex;
-    }
-
-    @Override
-    public void run() {
-        String message = "";
-        do {
-            try {
-                message = e.getMessage();
-                System.out.println(message);
-            } catch (Exception e) {
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
-            }
-        } while (!message.equals("STOP"));
-    }
+    String description() default "";
 }
